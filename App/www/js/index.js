c03e0280eccb6eefc7c676d9553e2a0c03d6187c
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-05 16:42:37 +0200

    Poprawki w index.html i index.js DZIAŁA LOGOWANIE PRZEZ EMAIL i GOOGLE

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -35,6 +35,15 @@ function login(){
 });
 
 }
+function register (){
+		var userEmail = document.getElementById("email_field").value;
+		var userPass = document.getElementById("password_field").value;
+			
+			firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+			console.log(error);
+});
+	
+}
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
@@ -67,24 +76,14 @@ function logout(){
 function login_facebook(){
 	    var provider = new firebase.auth.FacebookAuthProvider();
 	
-				firebase.auth().getRedirectResult().then(function(result) {
-				  if (result.credential) {
-					// This gives you a Facebook Access Token. You can use it to access the Facebook API.
-					var token = result.credential.accessToken;
-					// ...
-				  }
-				  // The signed-in user info.
-				  var user = result.user;
-				}).catch(function(error) {
-				  // Handle Errors here.
-				  var errorCode = error.code;
-				  var errorMessage = error.message;
-				  // The email of the user's account used.
-				  var email = error.email;
-				  // The firebase.auth.AuthCredential type that was used.
-				  var credential = error.credential;
-				  // ...
-				});
+
+firebase.auth().signInWithRedirect(provider);
+
+firebase.auth().getRedirectResult().then(function(authData) {
+	console.log(authData);
+}).catch(function(error) {
+	console.log(error);
+});
 }
 
 function login_google(){
