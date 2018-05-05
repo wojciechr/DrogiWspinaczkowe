56f0c15734451b1a364f0000c76fc2fca7d94a1a
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-05 15:06:18 +0200

    Poprawki w index.js DZIAŁA LOGOWANIE PRZEZ EMAIL

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -46,7 +46,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 		
 		var email_id=user.email;
 		
-		document.getElementById("user_para").innerHTML = "Welcome User : " + user.email 
+		document.getElementById("user_para").innerHTML = "Witaj : " + user.email 
 	}
 	
   } else {
@@ -92,21 +92,32 @@ function login_facebook(){
 function login_google(){
 
 		var provider = new firebase.auth.GoogleAuthProvider();
-		firebase.auth().signInWithPopup(provider).then(function(result) {
-		  // This gives you a Google Access Token. You can use it to access the Google API.
-		  var token = result.credential.accessToken;
-		  // The signed-in user info.
-		  var user = result.user;
-
-		  // ...
-		}).catch(function(error) {
-		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
-		  // The email of the user's account used.
-		  var email = error.email;
-		  // The firebase.auth.AuthCredential type that was used.
-		  var credential = error.credential;
-		  // ...
-		});
+			firebase.auth().signInWithRedirect(provider).then(function() {
+			  return firebase.auth().getRedirectResult();
+			}).then(function(result) {
+			  // This gives you a Google Access Token.
+			  // You can use it to access the Google API.
+			  var token = result.credential.accessToken;
+			  // The signed-in user info.
+			  var user = result.user;
+			  // ...
+			}).catch(function(error) {
+			  // Handle Errors here.
+			  var errorCode = error.code;
+			  var errorMessage = error.message;
+			});
+			firebase.auth().getRedirectResult().then(function(result) {
+			  if (result.credential) {
+				// This gives you a Google Access Token.
+				// You can use it to access the Google API.
+				var token = result.credential.accessToken;
+				// The signed-in user info.
+				var user = result.user;
+				// ...
+			  }
+			}).catch(function(error) {
+			  // Handle Errors here.
+			  var errorCode = error.code;
+			  var errorMessage = error.message;
+			});
 }
