21ff86e2d59276b5fce393f0951f3dfbd68f1448
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-05 15:55:06 +0200

    Poprawki w config.xml i index.js DZIAŁA LOGOWANIE PRZEZ EMAIL 3

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -92,22 +92,33 @@ function login_facebook(){
 function login_google(){
 
 		var provider = new firebase.auth.GoogleAuthProvider();
-				firebase.auth().getRedirectResult().then(function(result) {
-				  if (result.credential) {
-					// This gives you a Google Access Token. You can use it to access the Google API.
-					var token = result.credential.accessToken;
-					// ...
-				  }
+				firebase.auth().signInWithRedirect(provider).then(function() {
+				  return firebase.auth().getRedirectResult();
+				}).then(function(result) {
+				  // This gives you a Google Access Token.
+				  // You can use it to access the Google API.
+				  var token = result.credential.accessToken;
 				  // The signed-in user info.
 				  var user = result.user;
+				  // ...
 				}).catch(function(error) {
 				  // Handle Errors here.
 				  var errorCode = error.code;
 				  var errorMessage = error.message;
-				  // The email of the user's account used.
-				  var email = error.email;
-				  // The firebase.auth.AuthCredential type that was used.
-				  var credential = error.credential;
-				  // ...
 				});
+				firebase.auth().getRedirectResult().then(function(result) {
+				  if (result.credential) {
+					// This gives you a Google Access Token.
+					// You can use it to access the Google API.
+					var token = result.credential.accessToken;
+					// The signed-in user info.
+					var user = result.user;
+					// ...
+				  }
+				}).catch(function(error) {
+				  // Handle Errors here.
+				  var errorCode = error.code;
+				  var errorMessage = error.message;
+				});
+				
 }
