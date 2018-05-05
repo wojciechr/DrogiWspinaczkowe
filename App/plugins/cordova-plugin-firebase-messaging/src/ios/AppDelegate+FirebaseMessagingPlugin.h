0678c798c919c4f7e5b09168460cef8af8f4c795
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-05 09:40:03 +0200

    Poprawki w config.xml 2

--- App/plugins/cordova-plugin-firebase-messaging/src/ios/AppDelegate+FirebaseMessagingPlugin.h
+++
@@ -1,9 +0,0 @@
-#import "AppDelegate.h"
-
-@import FirebaseMessaging;
-
-@interface AppDelegate (FirebaseMessagingPlugin) <FIRMessagingDelegate>
-
-- (void)postNotification:(NSDictionary*) userInfo background:(BOOL) value;
-
-@end
