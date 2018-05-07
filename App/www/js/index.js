5f2f221cb2d23a5bada4590285ab0febf6c54f16
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-07 19:10:32 +0200

    Poprawki do index.js facebook redirect 5

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -77,13 +77,26 @@ function login_facebook(){
 	    var provider = new firebase.auth.FacebookAuthProvider();
 	
 
-firebase.auth().signInWithRedirect(provider);
+		firebase.auth().signInWithRedirect(provider);
 
-firebase.auth().getRedirectResult().then(function(authData) {
-	console.log(authData);
-}).catch(function(error) {
-	console.log(error);
-});
+		firebase.auth().getRedirectResult().then(function(result) {
+		  if (result.credential) {
+			// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+			var token = result.credential.accessToken;
+			// ...
+		  }
+		  // The signed-in user info.
+		  var user = result.user;
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
 }
 
 function login_google(){
