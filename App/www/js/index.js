1ed5b32186e47a4dde02b855009ffc9014bb45db
Szymon Pasierbek <szymonpasierbek@gmail.com>
2018-05-16 20:28:16 +0200

    jakas tam proba

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -25,7 +25,7 @@ function login_google() {
 
     firebase.auth().signInWithRedirect(provider);
 
-    firebase.auth().getRedirectResult().then(function(result) {
+    firebase.auth().getRedirectResult().then(function (result) {
         if (result.credential) {
             // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
@@ -35,7 +35,7 @@ function login_google() {
         var user = result.user;
         var b = document.getElementById('login_google');
         b.style.color = 'red';
-    }).catch(function(error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -45,10 +45,38 @@ function login_google() {
         var credential = error.credential;
         // ...
     });
+
+
 }
 
 
 function initApp() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+
+        //console.log(user);
+        var user = firebase.auth().currentUser;
+
+        if (user) {
+
+            var displayName = user.displayName;
+            var email = user.email;
+
+            document.getElementById("user_div").style.display = "block";
+            document.getElementById("login_div").style.display = "none";
+
+
+        } else {
+
+            document.getElementById("user_div").style.display = "none";
+            document.getElementById("login_div").style.display = "block";
+
+        }
+
+    });
+
+    document.getElementById('login_google').addEventListener('click', login_google, false);
+    document.getElementById('login_facebook').addEventListener('click', login_facebook_function, false);
 
 }
 
