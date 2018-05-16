3098489278a463b172452ce6f20a8d581709f6c0
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-16 21:59:19 +0200

    nowy plugin fb

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -9,7 +9,7 @@ const config = {
   firebase.initializeApp(config);
 
 
-  window.fbAsyncInit = function() {
+ /*window.fbAsyncInit = function() {
     FB.init({
       appId      : '2065286753691545',
       cookie     : true,
@@ -17,8 +17,8 @@ const config = {
       version    : 'v2.12'
     });
     FB.AppEvents.logPageView();   
--      
--  };
+     
+  };
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -26,7 +26,7 @@ const config = {
   js = d.createElement(s); js.id = id;
   js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.0&appId=2065286753691545&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));*/
 
 function login(){
 		var userEmail = document.getElementById("email_field").value;
@@ -62,27 +62,44 @@ function logout(){
 	
 }
 function login_facebook_function(){
-                var provider = new firebase.auth.FacebookAuthProvider();
-                // alert("Facebook signin");
-                if (firebase.auth().currentUser) {
-                // [START signout]
-                firebase.auth().signOut();
-				document.getElementById('login_facebook').textContent = "Facebook"
+facebookConnectPlugin.getLoginStatus(
+            function (status) {
+                console.log("current status: " + JSON.stringify(status));
+            },
+            function (error) {
+                console.log("Something went wrong: " + JSON.stringify(error));
+            }
+	 );
+	 
+	facebookConnectPlugin.login(["email"],function(result){
+			console.log("logowanie:");
+			 console.log("RESULT:" + JSON.stringify(result));
+			  console.log("RESULT2:" + JSON.stringify(result.authResponse));
+			  console.log("RESULT3:" + JSON.stringify(result.authResponse.accessToken));
+			  
+				firebase.auth().signInWithCredential(firebase.auth.FacebookAuthProvider.credential(result.authResponse.accessToken))
+					.then((success) => {
+						console.log("success: " + JSON.stringify(success)); 
+				   })
+				  
+		//calling api after login success
+		 facebookConnectPlugin.api("/me?fields=email,name,picture",["public_profile","email"]
+		 ,function(userData){
+			 //API success callback
+			 console.log(JSON.stringify(userData));
+		  },function(error){
+			 //API error callback
+			 console.log(JSON.stringify(error));
+		  });
+	   },function(error){
+		  //authenication error callback
+		 console.log(JSON.stringify(error));
+		 });
+	
+	
 
-                 //[END signout]
-                } else {
-                    firebase.auth().signInWithRedirect(provider).then(function(result) {
-						document.getElementById('login_facebook').textContent = "Facebook"
-                        var token = result.credential.accessToken;
-                        var user = result.user;         
-                       // console.log(token)
-                       // console.log(user)
-                    }).catch(function(error) {
-                       // console.log(error.code);
-                       // console.log(error.message);
-                    });
-                }
 }
+
 
 
 function login_google_function() {
