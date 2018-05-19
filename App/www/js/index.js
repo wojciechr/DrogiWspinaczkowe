0d3f7c45468001bafbfc9dd00c579873d8384db6
wojciechr <wojciech.rokosz91@gmail.com>
2018-05-19 16:10:15 +0200

    inne podejście

--- App/www/js/index.js
+++ App/www/js/index.js
@@ -1,32 +1,45 @@
-const config = {
-    apiKey: "AIzaSyA-ejZDxLBfqxuqcsakiY31p6iXt4cJc8M",
-    authDomain: "drogi-wspinaczkowe.firebaseapp.com",
-    databaseURL: "https://drogi-wspinaczkowe.firebaseio.com",
-    projectId: "drogi-wspinaczkowe",
-    storageBucket: "drogi-wspinaczkowe.appspot.com",
-    messagingSenderId: "81802368109"
-  };
-  firebase.initializeApp(config);
+function init() {
+	document.addEventListener('deviceready', onDeviceReady, false);
+}
 
+function onDeviceReady() {
+}
+const btnGoogleLogin = document.getElementById('btnGoogleLogin');
+const login_facebook = document.getElementById('login_facebook');
 
- /*window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '2065286753691545',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.12'
-    });
-    FB.AppEvents.logPageView();   
-     
-  };
+//Login with Google
+btnGoogleLogin.addEventListener('click', e => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider)
+        .then(function () {
+            return firebase.auth().getRedirectResult();
+        }).then(function (result) {
+            window.location.href = "index.html";
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.0&appId=2065286753691545&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));*/
+        }).catch(function (error) {
+            alert(error.message);
+
+        });
+});
+login_facebook.addEventListener('click', e => {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+        .then(function () {
+            var provider = new firebase.auth.FacebookAuthProvider();
+            firebase.auth().signInWithRedirect(provider).then(function () {
+                return firebase.auth().getRedirectResult();
+            }).then(function (result) {
+                if (result.credential) {
+                    window.location.href = "index.html";
+
+                }
+                var user = result.user;
+            }).catch(function (error) {
+                if (error.code === "auth/account-exists-with-different-credential") {
+                    alert("This email is already used!");
+                }
+            });
+        });
+});
 
 function login(){
 		var userEmail = document.getElementById("email_field").value;
@@ -36,6 +49,7 @@ function login(){
 
 		var errorCode = error.code;
 		var errorMessage = error.message;
+		
 
 
 
@@ -61,84 +75,7 @@ function logout(){
 	});
 }
 
-
-function login_facebook_function(){
-facebookConnectPlugin.getLoginStatus(
-            function (status) {
-                console.log("current status: " + JSON.stringify(status));
-            },
-            function (error) {
-                console.log("Something went wrong: " + JSON.stringify(error));
-            }
-	 );
-	 
-	facebookConnectPlugin.login(["email"],function(result){
-			console.log("logowanie:");
-			 console.log("RESULT:" + JSON.stringify(result));
-			  console.log("RESULT2:" + JSON.stringify(result.authResponse));
-			  console.log("RESULT3:" + JSON.stringify(result.authResponse.accessToken));
-			  
-				firebase.auth().signInWithCredential(firebase.auth.FacebookAuthProvider.credential(result.authResponse.accessToken))
-					.then((success) => {
-						console.log("success: " + JSON.stringify(success)); 
-				   })
-				  
-		//calling api after login success
-		 facebookConnectPlugin.api("/me?fields=email,name,picture",["public_profile","email"]
-		 ,function(userData){
-			 //API success callback
-			 console.log(JSON.stringify(userData));
-		  },function(error){
-			 //API error callback
-			 console.log(JSON.stringify(error));
-		  });
-	   },function(error){
-		  //authenication error callback
-		 console.log(JSON.stringify(error));
-		 });
-	
-	
-
-}
-
-
-
-
-   $('#logingoogle').click(function () {
-        window.plugins.googleplus.login(
-            {
-                'webClientId': '81802368109-8pomelv41akmrkb2vrmu6fhuch8bet5s.apps.googleusercontent.com',
-                'offline': true
-            },
-            function (obj) {
-
-                console.log(obj);
-                if (!firebase.auth().currentUser) {
-                    console.log(obj.idToken);
-                    firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(obj.idToken))
-                        .then((success) => {
-                    console.log("success: " + JSON.stringify(success)); // to long json to put it in #feedback
-                })
-                .catch((error) => {
-                        console.log("error0: " + JSON.stringify(error));
-                      });
-            }else{
-                console.log('error1: already sigend in firebase');
-            }
-        },
-        function (msg) {
-          console.log("error2: " + msg);
-}
-        );
-
-    });
- 		
-             
- 							
-
-
-			
-			
+		
 
 firebase.auth().onAuthStateChanged(function(user) {
 
@@ -192,7 +129,6 @@ firebase.auth().onAuthStateChanged(function(user) {
                 }
 
             });
-
 
 
      
