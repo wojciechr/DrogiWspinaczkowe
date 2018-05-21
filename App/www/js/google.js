11761e382406fa0828ff158bd2fa25d0da3073d1
Wojciech Rokosz <wojciech.rokosz91@gmail.com>
2018-05-21 14:01:55 +0200

    Logowanie przez Google

--- App/www/js/google.js
+++ App/www/js/google.js
@@ -1,40 +1,30 @@
 
 
 function google_login(){
-        var glprovider = new firebase.auth.GoogleAuthProvider(); 
-		firebase.auth().signInWithRedirect(glprovider).then(function(result) {
-		  // This gives you a Google Access Token. You can use it to access the Google API.
-		  var token = result.credential.accessToken;
-		  // The signed-in user info.
-		  var user = result.user;
-		  // ...
-		}).catch(function(error) {
-		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
-		  alert(errorMessage);
-		  // The email of the user's account used.
-		  var email = error.email;
-		  // The firebase.auth.AuthCredential type that was used.
-		  var credential = error.credential;
-		  // ...
-		}); 
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token.
-    // You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  alert(errorMessage);
-});
+ 		var provider = new firebase.auth.GoogleAuthProvider();
+ 		provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+ 			   firebase.auth().signInWithRedirect(provider);
+ 				firebase.auth().getRedirectResult().then(function(result)  {
+ 					if (result.credential) {
+ 					  var token = result.credential.accessToken;
+ 					  document.getElementById('quickstart-oauthtoken').textContent = token;
+ 					} else {
+ 					  document.getElementById('quickstart-oauthtoken').textContent = 'null';
+ 					}
+ 					var user = result.user;
+ 				  }).catch(function(error) {
+ 					var errorCode = error.code;
+ 					var errorMessage = error.message;
+ 					 var email = error.email;
+ 					var credential = error.credential;
+ 					if (errorCode === 'auth/account-exists-with-different-credential') {
+ 					  alert('You have already signed up with a different auth provider for that email.');
+ 					  // If you are using multiple auth providers on your app you should handle linking
+ 					  // the user's accounts here.
+ 					} else {
+ 					  console.error(error);
+ 					}
+ 				  });
 
 
 		
