c8021260f15553d3fad760692d65913694243230
Tomek Romanowicz <tomromanowicz@gmail.com>
2018-05-25 17:13:56 +0200

    usun platform, niepotrzebne

--- App/platforms/android/app/src/main/assets/www/img/readme.txt
+++
@@ -1,1 +0,0 @@
-Image files... eg. icons, splashscreen
--- App/platforms/android/app/src/main/assets/www/js/register.js
+++
@@ -1,9 +0,0 @@
-function register (){
-		var userEmail = document.getElementById("email_field").value;
-		var userPass = document.getElementById("password_field").value;
-			
-			firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-			console.log(error);
-});
-	
-}
