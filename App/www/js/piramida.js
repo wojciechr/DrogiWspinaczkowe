07b17624d192cb902897aaee52f9ee9068f8403a
Szymon Pasierbek <szymonpasierbek@gmail.com>
2018-06-10 21:39:51 +0200

    piramida - chyba juz dziala

--- App/www/js/piramida.js
+++ App/www/js/piramida.js
@@ -96,14 +96,86 @@ function getData() {
             }]
         };
 
+
+
+        var barOptions_stacked = {
+            tooltips: {
+                enabled: false
+            },
+            hover: {
+                animationDuration: 0
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        fontFamily: "'Open Sans Bold', sans-serif",
+                        fontSize: 11
+                    },
+                    scaleLabel: {
+                        display: false
+                    },
+                    gridLines: {},
+                    stacked: true
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display: false,
+                        color: "#fff",
+                        zeroLineColor: "#fff",
+                        zeroLineWidth: 0
+                    },
+                    ticks: {
+                        fontFamily: "'Open Sans Bold', sans-serif",
+                        fontSize: 11
+                    },
+                    stacked: true
+                }]
+            },
+            legend: {
+                display: false
+            },
+
+            animation: {
+                onComplete: function () {
+                    var chartInstance = this.chart;
+                    var ctx = chartInstance.ctx;
+                    ctx.textAlign = "left";
+                    ctx.font = "9px Open Sans";
+                    ctx.fillStyle = "#fff";
+
+                    Chart.helpers.each(this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+                        Chart.helpers.each(meta.data.forEach(function (bar, index) {
+                            data = dataset.data[index];
+                            /*
+                    if(i==0){
+                                    ctx.fillText(data, 50, bar._model.y+4);
+                              } else {
+                                    ctx.fillText(data, bar._model.x-25, bar._model.y+4);
+                              }
+                    */
+                        }), this)
+                    }), this);
+                }
+            },
+            pointLabelFontFamily: "Quadon Extra Bold",
+            scaleFontFamily: "Quadon Extra Bold",
+        };
+
+        window.alert('setChart');
         window.alert(typeof(data));
         window.alert(data);
 
-        return data;
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: data,
+            options: barOptions_stacked,
+        });
 
     }, function (error) {
         console.log("Error: " + error.code);
     });
-
 
 }
