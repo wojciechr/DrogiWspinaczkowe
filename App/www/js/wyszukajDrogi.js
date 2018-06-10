e68c3af0d24c45036d2a454455ed6263bcc71abf
Szymon Pasierbek <szymonpasierbek@gmail.com>
2018-06-10 13:40:46 +0200

    tabelka dziala

--- App/www/js/wyszukajDrogi.js
+++ App/www/js/wyszukajDrogi.js
@@ -14,70 +14,30 @@ firebase.initializeApp(config);
 // }
 
 function wyszukajDrogi() {
-    var logbox = document.getElementById("logbox");
-
-    logbox.value = "1";
-
     var userId;
 
-    logbox.value = "2";
     var user = firebase.auth().currentUser;
-    logbox.value = "3";
     if (user) {
         userId = user.uid;
-        logbox.value = "4";
     } else {
-        logbox.value = "5";
         window.alert("Nie zalogowałeś się!");
         return;
     }
-    logbox.value = "6";
     var userPath = "Users/" + userId;
-    logbox.value = "7";
     var firebaseRef = firebase.database().ref().child(userPath);
 
-    logbox.value = "88888";
-
     firebaseRef.on("value", function (snapshot) {
-        console.log(snapshot.val());
 
-    // snapshot = {
-    //     "Trasa01": {
-    //         "Data": "2018-06-08",
-    //         "NazwaDrogi": "Skurwysyn",
-    //         "NazwaRegionu": "Tyniec",
-    //         "NazwaSkały": "Skurwysyn",
-    //         "StylSrzejścia": "OS",
-    //         "Trudność": "7a+",
-    //         "Typ": "Skały"
-    //     },
-    //     "Trasa02": {
-    //         "Data": "2018-06-08",
-    //         "NazwaDrogi": "Skurwysyn",
-    //         "NazwaRegionu": "Tyniec",
-    //         "NazwaSkały": "Przdskurwysyn",
-    //         "StylSrzejścia": "RP",
-    //         "Trudność": "7b",
-    //         "Typ": "Ścianka"
-    //     },
-    //     "Trasa03": {
-    //         "Data": "2018-06-08",
-    //         "NazwaDrogi": "Wawrzynegger",
-    //         "NazwaRegionu": "Tyniec",
-    //         "NazwaSkały": "Skurwysyn",
-    //         "StylSrzejścia": "Proba",
-    //         "Trudność": "8a",
-    //         "Typ": "Skały"
-    //     }
-    // };
-        var routes = snapshot.val();
-        // logbox.value = JSON.stringify(routes.val());
+        var typ = document.getElementById("typ_field").value;
 
-        for (var route_key in routes) {
-            if (routes.hasOwnProperty(route_key)) {
-                $("body").append("1 ");
-            }
+        for (var route_key in snapshot.val()) {
             route = routes[route_key];
+
+            if (typ != "Wszystkie") {
+                if (typ != route.Typ) {
+                    continue;
+                }
+            }
 
             var table = document.getElementById("wyniki_wyszukiwania");
             var row = table.insertRow(1);
@@ -89,15 +49,12 @@ function wyszukajDrogi() {
             var trudnosc = row.insertCell(4);
             var data = row.insertCell(5);
 
-            $("body").append("2 ");
-
             droga.innerHTML = route.NazwaDrogi;
             skala.innerHTML = route.NazwaSkały;
             region.innerHTML = route.NazwaRegionu;
             styl.innerHTML = route.StylSrzejścia;
             trudnosc.innerHTML = route.Trudność;
             data.innerHTML = route.Data;
-            $("body").append("3 ");
         }
 
     }, function (error) {
